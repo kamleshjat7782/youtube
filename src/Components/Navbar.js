2cddc0c669d2eb965105e48c1d680bc763c17b40
kamlesh choudhary <kamleshjat7782@gmail.com>
2023-10-13 17:55:07 +0530

    Update favicon and logo and add meta tag

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,9 @@
 import { Stack } from '@mui/system';
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { logo } from '../Utils/Constants';
+// import { logo } from '../Utils/Constants';
 import SearchBar from './SearchBar';
+import logo from '../images/logo.png'
 
 const Navbar = () => {
   return (
@@ -14,7 +15,7 @@ const Navbar = () => {
         sx={{ position: 'sticky', background: '#000', top: '0', }} 
         >
           <Link to='/youtube' className='logo'>
-            <img src={logo} alt='logo' />
+            <img src={logo} alt='logo' style={{padding: '0 10px 0 0'}} />
           </Link>
           <SearchBar/>
       </Stack>
